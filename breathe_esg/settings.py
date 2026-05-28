bb96c6cad987158a3b062b84cfd3dfcf55225bd4
Pandora <nishantaditya222@gmail.com>
2026-05-29 00:55:23 +0530

    Update settings.py

--- breathe_esg/settings.py
+++ breathe_esg/settings.py
@@ -79,11 +79,13 @@ WSGI_APPLICATION = 'breathe_esg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
